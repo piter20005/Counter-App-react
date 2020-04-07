f0b4eaebd74a321e77df7fa27b7976a7fabbd06d
piter20005 <piterst25@gmail.com>
2020-04-07 12:32:38 +0200

    Console log add

--- src/App.js
+++ src/App.js
@@ -7,47 +7,68 @@ import { Component } from "react";
 class App extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
+      {
+        id: 1,
+        value: 0,
+      },
+      {
+        id: 2,
+        value: 0,
+      },
+      {
+        id: 3,
+        value: 0,
+      },
+      {
+        id: 4,
+        value: 0,
+      },
+    ],
   };
 
-  handleIncrement = counter => {
+  handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
+    counters[index] = {
+      ...counter,
+    };
     counters[index].value++;
-    this.setState({ counters });
+    this.setState({
+      counters,
+    });
+    console.log("incremented");
   };
 
   handleReset = () => {
-    const counters = this.state.counters.map(c => {
+    const counters = this.state.counters.map((c) => {
       c.value = 0;
       return c;
     });
-    this.setState({ counters });
+    this.setState({
+      counters,
+    });
   };
 
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({
+      counters,
+    });
   };
   render() {
     return (
       <>
         <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
-        />
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />{" "}
         <main className="container">
           <Counters
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
             onDelete={this.handleDelete}
-          />
-        </main>
+          />{" "}
+        </main>{" "}
       </>
     );
   }
